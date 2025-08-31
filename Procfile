f1c894a0dc85c2a72cb47cbb8fa8e96e593551a0
Merrick1307 <mahmud150@icloud.com>
2025-08-31 19:04:30 +0100

    updated static file settings and added whitenoise for better swagger ui support

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: poetry run python manage.py migrate
+release: poetry run python manage.py migrate && poetry run python manage.py collectstatic --noinput
 web: gunicorn --bind 0.0.0.0:$PORT auth_service.wsgi:application
--- auth_service/settings.py
+++ auth_service/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,6 +141,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+# Static files settings
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
